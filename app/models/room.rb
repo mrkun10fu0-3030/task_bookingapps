691cda59588e806af5c90105746d71bf79c2d970
mrkun10fu0-3030 <yyamahara13@gmail.com>
2026-06-09 13:28:35 +0900

    Fix RuboCop offenses

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,9 +2,9 @@ class Room < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
   has_one_attached :image
-  
+
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :address, presence: true
-end 
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :rooms, dependent: :destroy
   has_many :reservations, dependent: :destroy
   has_one_attached :icon
